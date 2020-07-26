8777062728a716fabbcad1f9227ae9d6ab127495
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-26 22:23:46 +0530

    String to Integer

---
+++ edabit/return_string_as_an_integer.java
@@ -0,0 +1,15 @@
+package edabit;
+
+public class return_string_as_an_integer {
+	public static int stringInt(String str) {
+		int result = Integer.parseInt(str);
+		return result;
+		
+	}
+	public static void main(String[] args) {
+		return_string_as_an_integer i = new return_string_as_an_integer();
+		System.out.println("1"); //op:-1
+		
+	}
+
+}
