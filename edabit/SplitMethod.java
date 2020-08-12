2604f22e1c1fa7f8d83d3c4dc309bb4e386462f3
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-08-12 22:27:28 +0530

    Split Method

---
+++ edabit/SplitMethod.java
@@ -0,0 +1,19 @@
+package edabit;
+
+public class SplitMethod {
+public static void main(String[] args) {
+String str = "Sachin Tendulkar"; 
+//System.out.println(s.charAt(1));
+	
+	String[] result = str.split("");
+	
+	for(String s :result) {
+		System.out.println(s);
+	
+		
+	}
+	
+	
+}
+
+}
